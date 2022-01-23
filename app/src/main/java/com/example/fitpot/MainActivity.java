be90ef3d8a5396c0d0ccb4cfb7b670185340d5e0
unknown <pkl9pkl@gmail.com>
2022-01-23 16:47:03 +0100

    a

--- app/src/main/java/com/example/fitpot/MainActivity.java
+++ app/src/main/java/com/example/fitpot/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.fitpot;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
-import com.example.fitpot.ui.map.Map;
+import com.example.fitpot.ui.map.MapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,18 +19,13 @@ import com.example.fitpot.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private Gyroscope gyroscope;
-    private Map map;
     private ActivityMainBinding binding;
-    private Accelerometer accelerometer;
-    private double MagnitudePrevious = 0;
-    private Integer stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gyroscope = new Gyroscope(this);
-        accelerometer = new Accelerometer(this);
-        getFromShared();
+
         gyroscope.setListener(new Gyroscope.Listener() {
 
             @Override
@@ -50,19 +42,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-        });
-
-        accelerometer.setListener(new Accelerometer.Listener() {
-            @Override
-            public void onTranslation(float tx, float ty, float tz) {
-                double Magnitude = Math.sqrt(tx*tx + ty*ty + tz*tz);
-                double MagnitudeDelta = Magnitude - MagnitudePrevious;
-                MagnitudePrevious = Magnitude;
-                if (MagnitudeDelta > 1){
-                    stepCount++;
-                }
-                addToShared();
-            }
         });
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -84,57 +63,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
         gyroscope.register();
-        accelerometer.register();
     }
 
     @Override
     protected void onPause(){
         super.onPause();
         gyroscope.unregister();
-        accelerometer.unregister();
     }
 
-    void addToShared(){
-        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
-    void getFromShared(){
-        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
-        stepCount = sharedPreferences.getInt("stepCount", 0);
-    }
-
-    public void showToast(String message){
-        Toast.makeText(getApplicationContext(), message,
-                Toast.LENGTH_SHORT).show();
-    }
-
-    public void buyFromShop(String message, int amount){
-        getFromShared();
-        if(stepCount > amount){
-            stepCount -= amount;
-            addToShared();
-            showToast(message);
-        }
-        else
-        {
-            showToast(getString(R.string.no_money));
-        }
-    }
-
-    public void buyWaterUpgrade(View view){
-        buyFromShop(getString(R.string.purchase_1), 50);
-    }
-
-    public void buyPlantUpgrade(View view){
-        buyFromShop(getString(R.string.purchase_2), 500);
-    }
-
-    public void buyRunningUpgrade(View view){
-        buyFromShop(getString(R.string.purchase_3), 1000);
-    }
 
 }
