24fa6fb2b9c28e52b3427a4735cb841ac2943b5e
maz8569 <akarpetskiy82@gmail.com>
2022-01-27 22:10:18 +0100

    czasowe powiadomienia

--- app/src/main/java/com/example/fitpot/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/fitpot/ui/notifications/NotificationsFragment.java
@@ -15,6 +15,7 @@ import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,12 +30,20 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.fitpot.R;
 import com.example.fitpot.databinding.FragmentNotificationsBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
     private ToggleButton alarmToggle;
+    private ToggleButton alarmRepToggle;
+    private Button timePicker;
+    private MaterialTimePicker materialTimePicker;
+    private Calendar calendar;
 
     private NotificationManager mNotificationManager;
     private static final int NOTIFICATION_ID = 0;
@@ -52,11 +61,12 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         alarmToggle = binding.alarmToggle;
+        alarmRepToggle = binding.alarmRepToggle;
         Intent notifyIntent = new Intent(getActivity(), AlarmReceiver.class);
         boolean alarmUp = (PendingIntent.getBroadcast(getActivity(), NOTIFICATION_ID, notifyIntent,
                 PendingIntent.FLAG_NO_CREATE) != null);
         alarmToggle.setChecked(alarmUp);
-
+        timePicker =(Button) binding.setTime;
         final PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
                 (getActivity(), NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         final AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
@@ -88,7 +98,40 @@ public class NotificationsFragment extends Fragment {
                         alarmManager.cancel(notifyPendingIntent);
                     }
 
-                    mNotificationManager.cancelAll();
+                    //mNotificationManager.cancelAll();
+
+                }
+
+//Show a toast to say the alarm is turned on or off
+                Toast.makeText(getActivity(), toastMessage, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        alarmRepToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                String toastMessage;
+                if(b) {
+                    //Set the toast message for the "on" case
+                    toastMessage = getString(R.string.rp_al_set_up);
+
+                    if (alarmManager != null) {
+                        alarmManager.setInexactRepeating
+                                (AlarmManager.RTC_WAKEUP,
+                                        calendar.getTimeInMillis(),
+                                        AlarmManager.INTERVAL_DAY,
+                                        notifyPendingIntent);
+                    }
+                }else {
+                    //Set the toast message for the "off" case
+                    toastMessage = getString(R.string.rp_al_set_off);
+
+                    if (alarmManager != null) {
+                        alarmManager.cancel(notifyPendingIntent);
+                    }
+
+                    //mNotificationManager.cancelAll();
 
                 }
 
@@ -106,7 +149,56 @@ public class NotificationsFragment extends Fragment {
             }
         });
         createNotificationChannel();
+
+        timePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePicker();
+            }
+        });
+
         return root;
+    }
+
+    private void showTimePicker() {
+
+        materialTimePicker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Select Alarm Time")
+                .build();
+
+        materialTimePicker.show(getActivity().getSupportFragmentManager(), "time_picker");
+
+        materialTimePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(materialTimePicker.getHour() > 12)
+                {
+                    binding.alarmTime.setText(
+                            String.format("%02d", materialTimePicker.getHour()-12)
+                                    + " : "
+                                    + String.format("%02d", materialTimePicker.getMinute())
+                                    + "PM"
+                    );
+                }
+                else {
+                    binding.alarmTime.setText(
+                            String.format("%02d", materialTimePicker.getHour())
+                                    + " : "
+                                    + String.format("%02d", materialTimePicker.getMinute())
+                                    + "AM"
+                    );
+                }
+
+                calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY, materialTimePicker.getHour());
+                calendar.set(Calendar.MINUTE, materialTimePicker.getMinute());
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
+            }
+        });
     }
 
     @Override
